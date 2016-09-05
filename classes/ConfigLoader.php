41907d6a01a7572dcdbc5dd5c9c5808251143802
Hendrik <hendrik@4kalinke.de>
2016-09-05 13:02:41 +0200

    configloader

---
+++ classes/ConfigLoader.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Hendrik
+ * Date: 05.09.2016
+ * Time: 12:53
+ */
+class ConfigLoader {
+    private $config;
+
+    public function __construct($configFile) {
+        $this->config = parse_ini_file($configFile, TRUE);
+    }
+
+    public function getConfig() {
+        return $this->config;
+    }
+
+    public function getGroup($group) {
+        if(array_key_exists($group, $this->config))
+            return $this->config[$group];
+
+        return false;
+    }
+
+    public function getValue($group, $attribute) {
+        $gr = $this->getGroup($group);
+        if($gr===false) return false;
+
+        if(array_key_exists($attribute, $gr))
+            return $gr[$attribute];
+        return false;
+    }
+}
--- index.php
+++ index.php
@@ -5,3 +5,23 @@
  * Date: 05.09.2016
  * Time: 12:19
  */
+// includes
+include_once 'classes/ConfigLoader.php';
+
+// load config file
+$cfg = new ConfigLoader("config/mongo.ini");
+
+// connect to mongo server
+$mClient = new MongoClient("mongodb://".$cfg->getValue("ServerConnection", "ip").":".$cfg->getValue("ServerConnection", "port"));
+
+// connect to mongo db
+$db = new MongoDB($mClient, $cfg->getValue("ServerConnection", "db"));
+
+$list = $db->listCollections();
+echo "Anzahl Collections: " . count($list) . "<br>";
+foreach ($list as $collection) {
+    echo "Collection: " . $collection ."<br>";
+    foreach($collection->find() as $cur) {
+        var_dump($cur);
+    }
+}
